96f01022cde84fd3d0d7df81025fc02b97f28f8e
Tony Phondeth <phondeth@gmail.com>
2021-06-30 15:24:37 -0400

    function arguments

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,36 @@
-var playerMoney = 10;
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
 
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+
+// You can also log multiple values at once like this
+// console.log(playerName, playerAttack, playerHealth);
+// console.log(enemyNames);
+// console.log(enemyNames[0]);
+// console.log(enemyNames[1]);
+// console.log(enemyNames[2]);
+
+// for(var i = 0; i < enemyNames.length; i++) {
+//     console.log(enemyNames[i]);
+//     console.log(i);
+//     console.log(enemyNames[i] + " is at " + i + " index");
+// }
+
 // Function expression
-var fight = function() {
+var fight = function(enemyName) {
+    // fight function statements
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -65,4 +83,6 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 };
 
 // execute/call Function
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
